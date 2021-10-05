91935ef016bdcc425cc26fedbd97000fb8f8f9d4
Nikita <nicitazaletin@yandex.ru>
2021-10-05 16:30:54 +0300

    Тест для метода overwriteFile

--- src/test/java/CompleteDestructionTest.java
+++ src/test/java/CompleteDestructionTest.java
@@ -1,7 +1,13 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,5 +28,24 @@ class CompleteDestructionTest {
 
     @Test
     void overwriteFile() {
+
+        // Символы для перезаписи
+        String str = "1234";
+
+        // Создаём временный файл для тестирования
+        try {
+            File file = File.createTempFile("testFile",".txt");
+            FileWriter fileWriter = new FileWriter(file, false);
+
+            fileWriter.write(str);
+            fileWriter.close();
+
+            Assertions.assertEquals(str, (new FileReader(file).read()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
