e8d443716c7799de31c1993dbbcf383296d98043
Nikita <nicitazaletin@yandex.ru>
2021-10-05 16:16:16 +0300

    Добавил необходимые зависимости, описал необходимые методы

---
+++ src/main/java/CompleteDestruction.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.nio.file.Path;
+
+public class CompleteDestruction {
+    public static void main(String[] args) {
+
+    }
+
+    public void recursiveDelete(File file){
+
+    }
+
+    public void deleteFile(File file){
+
+    }
+
+    public void overwriteFile(File file, String str){
+
+    }
+
+}
---
+++ src/test/java/CompleteDestructionTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CompleteDestructionTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void recursiveDelete() {
+    }
+
+    @Test
+    void deleteFile() {
+    }
+
+    @Test
+    void overwriteFile() {
+    }
+}
